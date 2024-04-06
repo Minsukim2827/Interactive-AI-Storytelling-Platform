20050b6cec14e7ca4a002f8bbfaf38850b96b6d6
Minsu <m.kim2827@gmail.com>
2024-04-06 18:43:36 +1300

    installed tailwindcss

---
+++ frontend/frontend/src/components/tailwindtest.jsx
@@ -0,0 +1,15 @@
+import './../css/tailwind.css';
+import React from 'react';
+
+const TailwindTest = () => {
+    return (
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+            <div>
+                <h1 className="text-xl font-medium text-black">Welcome to Tailwind!</h1>
+                <p className="text-gray-500">You're ready to build something amazing.</p>
+            </div>
+        </div>
+    );
+};
+
+export default TailwindTest;
