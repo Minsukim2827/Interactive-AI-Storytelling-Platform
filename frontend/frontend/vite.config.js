3af77e3dfc34e6850be04305b42b6211f393133d
Minsu <m.kim2827@gmail.com>
2024-04-25 13:32:10 +1200

    updated workflow

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend/',
+  base: '/Interactive-AI-Storytelling-Platform',
   plugins: [react()],
   resolve: {
     alias: {
