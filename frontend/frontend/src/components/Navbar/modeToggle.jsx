ca150c95623b0f11e56c0d03b83432da65c6b24d
acfuns <1haolong.sun@gmail.com>
2024-04-26 13:57:05 +0800

    fix: display sun icon

--- frontend/frontend/src/components/Navbar/modeToggle.jsx
+++ frontend/frontend/src/components/Navbar/modeToggle.jsx
@@ -9,13 +9,13 @@ import {
 import { useTheme } from "@/components/Navbar/theme-provider";
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme();
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Sun className="text-black h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
