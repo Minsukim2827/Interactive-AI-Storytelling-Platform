ba147ddae02d2924aa8a37e38f3f82f5c0a029f2
joellmacias <107099926+joellmacias@users.noreply.github.com>
2024-05-20 11:24:40 +1200

    user log

--- frontend/frontend/src/components/ProfilePage/Bookmarks.jsx
+++ frontend/frontend/src/components/ProfilePage/Bookmarks.jsx
@@ -4,12 +4,21 @@ import { useAuth } from "./../AuthProvider"; // Import useAuth to access user co
 import ImageModal from "../Modals";
 
 const Bookmarks = () => {
+    const { user } = useAuth(); // Access user context
+    
     return (
-        <div class ="mt-12">
-            <h1>Bookmarks</h1>
-            {/* Add your bookmark content here */}
+        <div className="w-full flex justify-center">
+            {user ? (
+                <div className="w-11/12 max-w-screen-lg flex flex-col items-center">
+                    <h1 className="w-full text-center text-3xl font-bold my-8">Bookmarks</h1>
+                </div>
+            ) : (
+                // Display message if user is not logged in
+                <p>Please log in to access this page.</p>
+            )}
         </div>
     );
+
 };
 
 export default Bookmarks;
